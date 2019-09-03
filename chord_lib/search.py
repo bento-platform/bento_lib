7084cd7621b4e571b6221116334bd11506b2412b
David Lougheed <david.lougheed@gmail.com>
2019-09-03 11:04:08 -0400

    Add some search constants

---
+++ chord_lib/search.py
@@ -0,0 +1,9 @@
+SEARCH_OPERATIONS = ("eq", "lt", "le", "gt", "ge", "co")
+SQL_SEARCH_CONDITIONS = {
+    "eq": "=",
+    "lt": "<",
+    "le": "<=",
+    "gt": ">",
+    "ge": ">=",
+    "co": "LIKE"
+}
