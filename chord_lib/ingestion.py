a022a9abd3a37a4574a6c8a79d2366b2614df46a
David Lougheed <david.lougheed@gmail.com>
2019-09-24 15:54:06 -0400

    Add __all__ and fix some issues found via tests

--- chord_lib/ingestion.py
+++ chord_lib/ingestion.py
@@ -4,6 +4,19 @@ from typing import Optional
 from werkzeug.utils import secure_filename
 
 
+__all__ = [
+    "WORKFLOW_TYPE_FILE",
+    "WORKFLOW_TYPE_STRING",
+    "WORKFLOW_TYPE_ENUM",
+
+    "file_with_prefix",
+    "formatted_output",
+    "namespaced_input",
+    "make_output_params",
+    "find_common_prefix",
+]
+
+
 WORKFLOW_TYPE_FILE = "file"
 WORKFLOW_TYPE_STRING = "string"
 WORKFLOW_TYPE_ENUM = "enum"
@@ -25,7 +38,7 @@ def _output_string(string, output_params):
 def formatted_output(output: dict, output_params: dict):
     if output["type"] == WORKFLOW_TYPE_FILE:
         return _output_file_name(output["value"], output_params)
-    elif output["type"] in (WORKFLOW_TYPE_STRING, WORKFLOW_TYPE_ENUM):
+    elif output["type"] in (WORKFLOW_TYPE_STRING, WORKFLOW_TYPE_ENUM):  # TODO: Check enum values?
         return _output_string(output["value"], output_params)
     else:
         raise NotImplementedError
@@ -55,18 +68,26 @@ def make_output_params(workflow_id: str, workflow_params: dict, workflow_inputs:
 def find_common_prefix(base_path: str, workflow_metadata: dict, output_params: dict) -> Optional[int]:
     prefix = None
     for output in workflow_metadata["outputs"]:
-        if output["type"] == WORKFLOW_TYPE_FILE:
-            file_path = os.path.join(base_path, formatted_output(output, output_params))
-            if os.path.exists(file_path):
-                prefix = 1
+        # It only makes sense to deal with file outputs TODO: IS THIS TRUE?
+        if output["type"] != WORKFLOW_TYPE_FILE:
+            continue
+
+        file_path = os.path.join(base_path, formatted_output(output, output_params))
+        if os.path.exists(file_path):
+            prefix = 1
 
     # Increase the prefix until a suitable one has been found
     duplicate_exists = prefix is not None
     while duplicate_exists:
         duplicate_exists = False
-        for file in workflow_metadata["outputs"]:
+
+        for output in workflow_metadata["outputs"]:
+            # It only makes sense to deal with file outputs TODO: IS THIS TRUE?
+            if output["type"] != WORKFLOW_TYPE_FILE:
+                continue
+
             duplicate_exists = duplicate_exists or os.path.exists(
-                os.path.join(base_path, file_with_prefix(_output_file_name(file, output_params), prefix)))
+                os.path.join(base_path, file_with_prefix(formatted_output(output, output_params), prefix)))
 
         if duplicate_exists:
             prefix += 1
