58d0c0a17efaff8e6254b4988d788381a3470579
David Lougheed <david.lougheed@gmail.com>
2019-09-30 10:34:57 -0400

    Fix chord_lib version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as rf:
 
 setuptools.setup(
     name="chord_lib",
-    version="1.0.0",
+    version="0.1.0",
 
     python_requires=">=3.6",
     install_requires=["Werkzeug>=0.16.0,<1.0"],
