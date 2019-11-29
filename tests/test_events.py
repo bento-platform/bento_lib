028d610b92c6ca98754e0ebd8cbed8a03730aced
David Lougheed <david.lougheed@gmail.com>
2019-11-29 11:34:54 -0500

    Add events tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -8,58 +8,95 @@ TEST_SERVICE_EVENT = "test_service_event"
 TEST_DATA_TYPE = "data_type"
 TEST_DATA_TYPE_EVENT = "test_data_type_event"
 
+TEST_EVENT_SCHEMA = {"type": "string"}
 TEST_EVENT_BODY = "test"
 
 
 def test_registration():
-    chord_lib.events.register_service_event_type(TEST_SERVICE_EVENT)
+    r = chord_lib.events.register_service_event_type(TEST_SERVICE_EVENT, TEST_EVENT_SCHEMA)
+    assert r
     assert TEST_SERVICE_EVENT in chord_lib.events.get_service_event_types()
+    r = chord_lib.events.register_service_event_type(TEST_SERVICE_EVENT, TEST_EVENT_SCHEMA)
+    assert not r
 
-    chord_lib.events.register_data_type_event_type(TEST_DATA_TYPE_EVENT)
+    r = chord_lib.events.register_data_type_event_type(TEST_DATA_TYPE_EVENT, TEST_EVENT_SCHEMA)
+    assert r
     assert TEST_DATA_TYPE_EVENT in chord_lib.events.get_data_type_event_types()
+    r = chord_lib.events.register_data_type_event_type(TEST_DATA_TYPE_EVENT, TEST_EVENT_SCHEMA)
+    assert not r
 
     assert TEST_SERVICE_EVENT not in chord_lib.events.get_data_type_event_types()
     assert TEST_DATA_TYPE_EVENT not in chord_lib.events.get_service_event_types()
 
+    # Invalid schema
+    r = chord_lib.events.register_data_type_event_type("some_event", {
+        "type": "object",
+        "additionalProperties": 7
+    })
+    assert not r
+
 
 def test_service_events():
-    def handle_service_event(message):
-        event = json.loads(message["data"])
-        assert event["service"] == TEST_SERVICE
-        assert event["type"] == TEST_SERVICE_EVENT
-        assert event["data"] == TEST_EVENT_BODY
+    try:
+        def handle_service_event(message):
+            event = json.loads(message["data"])
+            assert event["service"] == TEST_SERVICE
+            assert event["type"] == TEST_SERVICE_EVENT
+            assert event["data"] == TEST_EVENT_BODY
 
-    chord_lib.events.add_handler(chord_lib.events.ALL_SERVICE_EVENTS, handle_service_event)
-    chord_lib.events.start_event_loop()
+        chord_lib.events.add_handler(chord_lib.events.ALL_SERVICE_EVENTS, handle_service_event)
+        chord_lib.events.start_event_loop()
 
-    r = chord_lib.events.publish_service_event(TEST_SERVICE, TEST_SERVICE_EVENT, TEST_EVENT_BODY)
-    assert r
+        r = chord_lib.events.publish_service_event(TEST_SERVICE, TEST_SERVICE_EVENT, TEST_EVENT_BODY)
+        assert r
 
-    # TODO: False r case
+        r = chord_lib.events.publish_service_event(TEST_SERVICE, "fake_event", TEST_EVENT_BODY)
+        assert not r
 
-    time.sleep(0.1)
+        r = chord_lib.events.publish_service_event(TEST_SERVICE, TEST_SERVICE_EVENT, {"bad": "body"})
+        assert not r
 
-    chord_lib.events.stop_event_loop()
+        # TODO: False r case
+
+        time.sleep(0.1)
+
+    finally:
+        chord_lib.events.stop_event_loop()
 
 
 def test_data_type_events():
-    def handle_data_type_event(message):
-        event = json.loads(message["data"])
-        assert event["data_type"] == TEST_DATA_TYPE
-        assert event["type"] == TEST_DATA_TYPE_EVENT
-        assert event["data"] == TEST_EVENT_BODY
+    try:
+        def handle_data_type_event(message):
+            event = json.loads(message["data"])
+            assert event["data_type"] == TEST_DATA_TYPE
+            assert event["type"] == TEST_DATA_TYPE_EVENT
+            assert event["data"] == TEST_EVENT_BODY
 
-    chord_lib.events.add_handler(chord_lib.events.ALL_DATA_TYPE_EVENTS, handle_data_type_event)
-    chord_lib.events.start_event_loop()
+        chord_lib.events.add_handler(chord_lib.events.ALL_DATA_TYPE_EVENTS, handle_data_type_event)
+        chord_lib.events.start_event_loop()
 
-    r = chord_lib.events.publish_data_type_event(TEST_DATA_TYPE, TEST_DATA_TYPE_EVENT, TEST_EVENT_BODY)
-    assert r
+        r = chord_lib.events.publish_data_type_event(TEST_DATA_TYPE, TEST_DATA_TYPE_EVENT, TEST_EVENT_BODY)
+        assert r
 
-    # TODO: False r case
+        # TODO: False r case
 
-    time.sleep(0.1)
+        time.sleep(0.1)
 
+    finally:
+        chord_lib.events.stop_event_loop()
+
+
+def test_premature_stop():
     chord_lib.events.stop_event_loop()
+
+
+def test_late_handler():
+    try:
+        chord_lib.events.start_event_loop()
+        r = chord_lib.events.add_handler(chord_lib.events.ALL_SERVICE_EVENTS, lambda _: None)
+        assert not r
+    finally:
+        chord_lib.events.stop_event_loop()
 
 
 # TODO: Verify cross-talk
