87da46f3fde2bb396adf0b0e7b93e87642d48f3e
David Lougheed <david.lougheed@gmail.com>
2019-12-12 13:35:21 -0500

    Add support for array types in ingestion, handle non-file output params

--- chord_lib/ingestion.py
+++ chord_lib/ingestion.py
@@ -17,12 +17,25 @@ __all__ = [
 ]
 
 
+WORKFLOW_TYPE_ARRAY_SUFFIX = "[]"
+
+
+def array_of_type(workflow_type: str):
+    return "{}{}".format(workflow_type, WORKFLOW_TYPE_ARRAY_SUFFIX)
+
+
 WORKFLOW_TYPE_FILE = "file"
+WORKFLOW_TYPE_FILE_ARRAY = array_of_type(WORKFLOW_TYPE_FILE)
 WORKFLOW_TYPE_STRING = "string"
+WORKFLOW_TYPE_STRING_ARRAY = array_of_type("string")
 WORKFLOW_TYPE_ENUM = "enum"
+WORKFLOW_TYPE_ENUM_ARRAY = array_of_type("enum")
 
 
-def file_with_prefix(file_path: str, prefix: int) -> str:
+def file_with_prefix(file_path: str, prefix: Optional[int]) -> str:
+    if prefix is None:
+        return file_path
+
     file_parts = os.path.splitext(file_path)
     return "".join(("{}_{}".format(prefix, file_parts[0]), file_parts[1]))
 
@@ -31,15 +44,27 @@ def _output_file_name(file_name, output_params):
     return secure_filename(file_name.format(**output_params))
 
 
+def _output_file_name_array(file_template, map_from_input, output_params):
+    return [secure_filename(file_template.format(v)) for v in output_params[map_from_input]]
+
+
 def _output_string(string, output_params):
     return string.format(**output_params)
+
+
+def _output_string_array(string_template, map_from_input, output_params):
+    return [string_template.format(v) for v in output_params[map_from_input]]
 
 
 def formatted_output(output: dict, output_params: dict):
     if output["type"] == WORKFLOW_TYPE_FILE:
         return _output_file_name(output["value"], output_params)
+    elif output["type"] == WORKFLOW_TYPE_FILE_ARRAY:
+        return _output_file_name_array(output["value"], output["map_from_input"], output_params)
     elif output["type"] in (WORKFLOW_TYPE_STRING, WORKFLOW_TYPE_ENUM):  # TODO: Check enum values?
         return _output_string(output["value"], output_params)
+    elif output["type"] in (WORKFLOW_TYPE_STRING_ARRAY, WORKFLOW_TYPE_ENUM_ARRAY):
+        return _output_string_array(output["value"], output["map_from_input"], output_params)
     else:
         raise NotImplementedError
 
@@ -52,44 +77,54 @@ def make_output_params(workflow_id: str, workflow_params: dict, workflow_inputs:
     output_params = {}
 
     for i, input_spec in enumerate(workflow_inputs):
-        if input_spec["type"] != WORKFLOW_TYPE_FILE:
-            # TODO: Handle non-file inputs
-            continue
+        if input_spec["type"] in (WORKFLOW_TYPE_FILE, WORKFLOW_TYPE_FILE_ARRAY):
+            # TODO: DOCS: Just file name without path...
+            # TODO: Separate params for full path / path without drop_box stuff?
 
-        # TODO: DOCS: Just file name without path...
-        # TODO: Separate params for full path / path without drop_box stuff?
+            ni = namespaced_input(workflow_id, input_spec["id"])
+            output_params[input_spec["id"]] = ([os.path.basename(f) for f in workflow_params[ni]]
+                                               if input_spec["type"].endswith(WORKFLOW_TYPE_ARRAY_SUFFIX)
+                                               else os.path.basename(workflow_params[ni]))
 
-        output_params[input_spec["id"]] = os.path.basename(
-            workflow_params[namespaced_input(workflow_id, input_spec["id"])])
+        elif input_spec["type"] in (WORKFLOW_TYPE_STRING, WORKFLOW_TYPE_STRING_ARRAY, WORKFLOW_TYPE_ENUM,
+                                    WORKFLOW_TYPE_ENUM_ARRAY):
+            output_params[input_spec["id"]] = workflow_params[namespaced_input(workflow_id, input_spec["id"])]
+
+        else:
+            raise NotImplementedError
 
     return output_params
 
 
+def get_file_paths_from_output(base_path, output, output_params, prefix=None):
+    fo = formatted_output(output, output_params)
+    return [os.path.join(base_path, file_with_prefix(fo, prefix))] if not isinstance(fo, list) \
+        else [os.path.join(base_path, file_with_prefix(f, prefix)) for f in fo]
+
+
 def find_common_prefix(base_path: str, workflow_metadata: dict, output_params: dict) -> Optional[int]:
+    # Increase the prefix until a suitable one has been found, if needed
+
     prefix = None
-    for output in workflow_metadata["outputs"]:
-        # It only makes sense to deal with file outputs TODO: IS THIS TRUE?
-        if output["type"] != WORKFLOW_TYPE_FILE:
-            continue
 
-        file_path = os.path.join(base_path, formatted_output(output, output_params))
-        if os.path.exists(file_path):
-            prefix = 1
-
-    # Increase the prefix until a suitable one has been found
-    duplicate_exists = prefix is not None
-    while duplicate_exists:
+    while True:
         duplicate_exists = False
 
         for output in workflow_metadata["outputs"]:
+            if duplicate_exists:
+                break
+
             # It only makes sense to deal with file outputs TODO: IS THIS TRUE?
-            if output["type"] != WORKFLOW_TYPE_FILE:
+            if output["type"] not in (WORKFLOW_TYPE_FILE, WORKFLOW_TYPE_FILE_ARRAY):
                 continue
 
-            duplicate_exists = duplicate_exists or os.path.exists(
-                os.path.join(base_path, file_with_prefix(formatted_output(output, output_params), prefix)))
+            for file_path in get_file_paths_from_output(base_path, output, output_params, prefix):
+                duplicate_exists = duplicate_exists or os.path.exists(file_path)
 
         if duplicate_exists:
-            prefix += 1
+            prefix = prefix + 1 if prefix is not None else 1
+            continue  # Go around again to find a better prefix
+
+        break
 
     return prefix
--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pytest
 
@@ -8,16 +9,34 @@ TEST_OUTPUT_PARAMS = {
     "vcf": "test.vcf"
 }
 
+TEST_OUTPUT_PARAMS_ARRAY = {
+    "vcfs": ["test.vcf", "test2.vcf"]
+}
+
 TEST_OUTPUT_FILE = {
     "id": "vcf_gz",
     "type": "file",
     "value": "a {vcf}.gz"
 }
 
+TEST_OUTPUT_FILE_ARRAY = {
+    "id": "vcf_gz",
+    "type": "file[]",
+    "map_from_input": "vcfs",
+    "value": "{}.gz"
+}
+
 TEST_OUTPUT_STRING = {
     "id": "vcf_gz",
     "type": "string",
     "value": "a {vcf}"
+}
+
+TEST_OUTPUT_STRING_ARRAY = {
+    "id": "vcf_gz",
+    "type": "string[]",
+    "map_from_input": "vcfs",
+    "value": "{}.gz"
 }
 
 TEST_OUTPUT_ENUM = {
@@ -30,14 +49,38 @@ TEST_WORKFLOW_PARAMS = {
     "test.vcf": "/tmp/does_not_exist.vcf"
 }
 
+TEST_WORKFLOW_PARAMS_ARRAY = {
+    "test.vcfs": ["/tmp/test1.vcf", "/tmp/test2.vcf"]
+}
+
 TEST_INPUT_FILE = {
     "id": "vcf",
     "type": "file"
 }
 
+TEST_INPUT_FILE_ARRAY = {
+    "id": "vcfs",
+    "type": "file[]"
+}
+
+TEST_INPUT_FILE_ARRAY_2 = {
+    "id": "vcfs_2",
+    "type": "file[]"
+}
+
 TEST_INPUT_STRING = {
     "id": "vcf",
     "type": "string"
+}
+
+TEST_INPUT_STRING_ARRAY = {
+    "id": "vcfs",
+    "type": "string[]"
+}
+
+TEST_INPUT_DOES_NOT_EXIST = {
+    "id": "vcfs",
+    "type": "does_not_exist"
 }
 
 
@@ -63,13 +106,54 @@ TEST_WORKFLOW_PARAMS_EXISTING_2 = {
     "test.vcf": os.path.join(TEST_DATA_PATH, "dup.txt")
 }
 
+TEST_WORKFLOW_PARAMS_EXISTING_3 = {
+    "test.vcfs": [
+        os.path.join(TEST_DATA_PATH, "a.txt"),
+        os.path.join(TEST_DATA_PATH, "b.txt"),
+    ],
+    "test.vcfs_2": [
+        os.path.join(TEST_DATA_PATH, "c.txt"),
+    ]
+}
+
+TEST_OUTPUT_FILE_ARRAY_EXISTING = {
+    "id": "vcfs_out",
+    "type": "file[]",
+    "map_from_input": "vcfs",
+    "value": "{}"
+}
+
+TEST_OUTPUT_FILE_ARRAY_EXISTING_2 = {
+    "id": "vcfs_out_2",
+    "type": "file[]",
+    "map_from_input": "vcfs_2",
+    "value": "{}"
+}
+
 
 def test_file_with_prefix():
+    assert file_with_prefix("test.test", None) == "test.test"
     assert file_with_prefix("test.test", 1) == "1_test.test"
 
 
 def test_formatted_file_output():
     assert formatted_output(TEST_OUTPUT_FILE, TEST_OUTPUT_PARAMS) == "a_test.vcf.gz"
+
+
+def test_formatted_file_array_output():
+    assert all(
+        o == "{}.gz".format(i)
+        for i, o in zip(TEST_OUTPUT_PARAMS_ARRAY["vcfs"],
+                        formatted_output(TEST_OUTPUT_FILE_ARRAY, TEST_OUTPUT_PARAMS_ARRAY))
+    )
+
+
+def test_formatted_string_array_output():
+    assert all(
+        o == "{}.gz".format(i)
+        for i, o in zip(TEST_OUTPUT_PARAMS_ARRAY["vcfs"],
+                        formatted_output(TEST_OUTPUT_STRING_ARRAY, TEST_OUTPUT_PARAMS_ARRAY))
+    )
 
 
 def test_formatted_string_output():
@@ -97,12 +181,26 @@ def test_make_output_params():
     assert len(list(output_params.keys())) == 1
     assert output_params["vcf"] == "does_not_exist.vcf"
 
-    # TODO: Eventually fix test when we no longer skip non-file outputs
-    output_params_2 = make_output_params("test", TEST_WORKFLOW_PARAMS, [TEST_INPUT_STRING])
-    assert len(list(output_params_2.keys())) == 0
+    output_params = make_output_params("test", TEST_WORKFLOW_PARAMS_ARRAY, [TEST_INPUT_FILE_ARRAY])
+    assert "vcfs" in output_params
+    assert len(list(output_params.keys())) == 1
+    assert json.dumps(output_params["vcfs"]) == json.dumps(["test1.vcf", "test2.vcf"])
 
-    output_params_3 = make_output_params("test", TEST_WORKFLOW_PARAMS, [])
-    assert len(list(output_params_3.keys())) == 0
+    output_params = make_output_params("test", TEST_WORKFLOW_PARAMS, [TEST_INPUT_STRING])
+    assert "vcf" in output_params
+    assert len(list(output_params.keys())) == 1
+    assert output_params["vcf"] == TEST_WORKFLOW_PARAMS["test.vcf"]
+
+    output_params = make_output_params("test", TEST_WORKFLOW_PARAMS_ARRAY, [TEST_INPUT_STRING_ARRAY])
+    assert "vcfs" in output_params
+    assert len(list(output_params.keys())) == 1
+    assert json.dumps(output_params["vcfs"]) == json.dumps(["/tmp/test1.vcf", "/tmp/test2.vcf"])
+
+    with pytest.raises(NotImplementedError):
+        make_output_params("test", TEST_WORKFLOW_PARAMS, [TEST_INPUT_DOES_NOT_EXIST])
+
+    output_params = make_output_params("test", TEST_WORKFLOW_PARAMS, [])
+    assert len(list(output_params.keys())) == 0
 
 
 def test_find_common_prefix():
@@ -122,3 +220,13 @@ def test_find_common_prefix():
     output_params_4 = make_output_params("test", TEST_WORKFLOW_PARAMS_EXISTING_2, [TEST_INPUT_FILE])
     prefix_4 = find_common_prefix(TEST_DATA_PATH, {"outputs": [TEST_OUTPUT_FILE_EXISTING]}, output_params_4)
     assert prefix_4 == 3
+
+    output_params = make_output_params("test", TEST_WORKFLOW_PARAMS_EXISTING_3, [
+        TEST_INPUT_FILE_ARRAY,
+        TEST_INPUT_FILE_ARRAY_2,
+    ])
+    prefix = find_common_prefix(TEST_DATA_PATH, {"outputs": [
+        TEST_OUTPUT_FILE_ARRAY_EXISTING,
+        TEST_OUTPUT_FILE_ARRAY_EXISTING_2,
+    ]}, output_params)
+    assert prefix == 3
