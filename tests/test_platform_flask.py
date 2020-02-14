2820c7cc8fad6b0e13194b73ccc369b250d36f2f
David Lougheed <david.lougheed@gmail.com>
2020-02-14 10:21:26 -0500

    Add flask error-wrapping code for registering generic handlers

--- tests/test_platform_flask.py
+++ tests/test_platform_flask.py
@@ -1,12 +1,22 @@
 import chord_lib.auth.flask_decorators as fd
+import chord_lib.responses.flask_errors as fe
 import pytest
 
 from flask import Flask
+from werkzeug.exceptions import BadRequest, NotFound
 
 
 @pytest.fixture
 def flask_client():
     application = Flask(__name__)
+
+    application.register_error_handler(Exception, fe.flask_error_wrap_with_traceback(fe.flask_internal_server_error))
+    application.register_error_handler(BadRequest, fe.flask_error_wrap(fe.flask_bad_request_error))
+    application.register_error_handler(NotFound, fe.flask_error_wrap(fe.flask_not_found_error))
+
+    @application.route("/500")
+    def r500():
+        raise Exception("help")
 
     @application.route("/test1")
     @fd.flask_permissions_any_user
@@ -30,6 +40,18 @@ def flask_client():
 def test_flask_forbidden_error(flask_client):
     # Turn CHORD permissions mode on to make sure we're getting real permissions checks
     fd.CHORD_PERMISSIONS = True
+
+    # non-existent endpoint
+
+    r = flask_client.get("/non-existent")
+    assert r.status_code == 404
+    assert r.get_json()["code"] == 404
+
+    # server error endpoint
+
+    r = flask_client.get("/500")
+    assert r.status_code == 500
+    assert r.get_json()["code"] == 500
 
     # /test1
 
