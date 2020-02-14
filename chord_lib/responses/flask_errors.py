9dcd5aeccb927aca68c1a669f48c4a7f9fa4e63c
David Lougheed <david.lougheed@gmail.com>
2020-02-14 10:21:26 -0500

    Add flask error-wrapping code for registering generic handlers

--- chord_lib/responses/flask_errors.py
+++ chord_lib/responses/flask_errors.py
@@ -1,9 +1,17 @@
+import sys
+import traceback
+
 from flask import jsonify
 from functools import partial
+from typing import Callable
+
 from .errors import *
 
 
 __all__ = [
+    "flask_error_wrap_with_traceback",
+    "flask_error_wrap",
+
     "flask_error",
 
     "flask_bad_request_error",
@@ -16,11 +24,36 @@ __all__ = [
 ]
 
 
+def flask_error_wrap_with_traceback(fn: Callable, service_name="CHORD Service") -> Callable:
+    """
+    Function to wrap flask_* error creators with something that supports the application.register_error_handler method,
+    while also printing a traceback.
+    :param fn: The flask error-generating function to wrap
+    :param service_name: The name of the service (for logging purposes)
+    :return: The wrapped function
+    """
+    # TODO: pass exception?
+    def handle_error(_e):
+        print(f"[{service_name}] Encountered error:", file=sys.stderr)
+        traceback.print_exc()
+        return fn()
+    return handle_error
+
+
+def flask_error_wrap(fn: Callable) -> Callable:
+    """
+    Function to wrap flask_* error creators with something that supports the application.register_error_handler method.
+    :param fn: The flask error-generating function to wrap
+    :return: The wrapped function
+    """
+    return lambda _e: fn()
+
+
 def flask_error(code: int, *errors):
     return jsonify(http_error(code, *errors)), code
 
 
-def _flask_error(code: int):
+def _flask_error(code: int) -> Callable:
     return partial(flask_error, code)
 
 
