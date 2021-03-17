c2d03dc22cd534cb2befb6a1a42cc7bc0fa2369d
David Lougheed <david.lougheed@gmail.com>
2021-03-17 14:32:43 -0400

    Finish drs tests

--- tests/test_drs.py
+++ tests/test_drs.py
@@ -1,7 +1,11 @@
 import json
+import pytest
+import requests_mock
 
+from bento_lib.responses import errors
 from bento_lib.drs import utils as drs_utils
 
+TEST_DRS_ID = "dd11912c-3433-4a0a-8a01-3c0699288bef"
 
 TEST_DRS_REPLY_NO_ACCESS = {
     "checksums": [{
@@ -10,9 +14,9 @@ TEST_DRS_REPLY_NO_ACCESS = {
     }],
     "created_time": "",  # TODO: RFC3339
     "updated_time": "",  # TODO: RFC3339
-    "id": "dd11912c-3433-4a0a-8a01-3c0699288bef",
+    "id": TEST_DRS_ID,
     "mime_type": "text/plain",
-    "self_uri": "drs://localhost/dd11912c-3433-4a0a-8a01-3c0699288bef",
+    "self_uri": f"drs://localhost/{TEST_DRS_ID}",
     "size": 4,
 }
 
@@ -29,13 +33,37 @@ def test_get_file_access_method():
     assert json.dumps(drs_utils.get_file_access_method_if_any(TEST_DRS_REPLY), sort_keys=True) == \
         json.dumps(TEST_DRS_REPLY["access_methods"][0], sort_keys=True)
 
+    assert drs_utils.get_file_access_method_if_any(TEST_DRS_REPLY_NO_ACCESS) is None
+
 
 def test_drs_uri_decode():
     assert drs_utils.decode_drs_uri("drs://example.org/abc") == "https://example.org/ga4gh/drs/v1/objects/abc"
     assert drs_utils.decode_drs_uri("drs://example.org/abc", internal_drs_base_url="http://localhost/sub/") == \
            "http://localhost/sub/ga4gh/drs/v1/objects/abc"
+
+    with pytest.raises(drs_utils.DrsInvalidScheme):
+        drs_utils.decode_drs_uri("http://example.org/abc")
+
     # TODO: Really, Bento should pass ga4gh/drs URLs to DRS directly instead of under a sub-path
 
 
 def test_drs_uri_fetch():
-    pass
+    with requests_mock.Mocker() as m:
+        m.get(f"https://example.org/ga4gh/drs/v1/objects/{TEST_DRS_ID}", json=TEST_DRS_REPLY)
+        m.get(f"http://localhost/ga4gh/drs/v1/objects/{TEST_DRS_ID}", json=TEST_DRS_REPLY)
+
+        m.get("https://example.org/ga4gh/drs/v1/objects/abc", status_code=404, json=errors.not_found_error())
+        m.get("http://localhost/ga4gh/drs/v1/objects/abc", status_code=404, json=errors.not_found_error())
+
+        assert json.dumps(drs_utils.fetch_drs_record_by_uri(f"drs://example.org/{TEST_DRS_ID}"), sort_keys=True) == \
+            json.dumps(TEST_DRS_REPLY, sort_keys=True)
+        assert json.dumps(drs_utils.fetch_drs_record_by_uri(
+            f"drs://example.org/{TEST_DRS_ID}",
+            internal_drs_base_url="http://localhost/"
+        ), sort_keys=True) == json.dumps(TEST_DRS_REPLY, sort_keys=True)
+
+        with pytest.raises(drs_utils.DrsRequestError):
+            drs_utils.fetch_drs_record_by_uri("drs://example.org/abc")
+
+        with pytest.raises(drs_utils.DrsRequestError):
+            drs_utils.fetch_drs_record_by_uri("drs://example.org/abc", internal_drs_base_url="http://localhost/")
