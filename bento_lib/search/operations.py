ec42d47fc4a70e34f770c52e16f3a84a603172b4
Paul Pillot <paul.pillot@mcgill.ca>
2022-08-06 21:48:54 -0400

    add `in` to the search operations
    (cherry picked from commit 8eac76a2e005c65bcffc463e80264a8e51175f73)

--- bento_lib/search/operations.py
+++ bento_lib/search/operations.py
@@ -8,6 +8,8 @@ __all__ = [
     "SEARCH_OP_CO",
     "SEARCH_OP_ICO",
 
+    "SEARCH_OP_IN",
+
     "SEARCH_OPERATIONS",
 ]
 
@@ -21,12 +23,15 @@ SEARCH_OP_GE = "ge"
 SEARCH_OP_CO = "co"
 SEARCH_OP_ICO = "ico"
 
+SEARCH_OP_IN = "in"
+
 SEARCH_OPERATIONS = (
     SEARCH_OP_EQ,
     SEARCH_OP_LT,
     SEARCH_OP_LE,
     SEARCH_OP_GT,
     SEARCH_OP_GE,
+    SEARCH_OP_IN,
     SEARCH_OP_CO,
     SEARCH_OP_ICO,
 )
