b6dce3a8836266b389636564dfb884e642c00267
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-02-09 00:05:59 -0500

    chore: introduced optional-authn token decorator

--- bento_lib/auth/middleware.py
+++ bento_lib/auth/middleware.py
@@ -39,45 +39,52 @@ class AuthxFlaskMiddleware():
 
             time.sleep(60)  # sleep 1 minute
 
-    def verify_token(self):
+    def verify_token_optional(self):
         if request.headers.get("Authorization"):
-            # Assume is Bearer token
-            authz_str_split = request.headers.get("Authorization").split(' ')
-            if len(authz_str_split) > 1:
-                token_str = authz_str_split[1]
-                # print(token_str)
+            self.verify_token()
 
-                # use idp public_key to validate and parse inbound token
-                try:
-                    payload = jwt.decode(token_str, self.public_key, algorithms=[self.oidc_alg], audience="account")
-                    # header = jwt.get_unverified_header(token_str)
-                except jwt.exceptions.ExpiredSignatureError:
-                    raise AuthXException('Expired access_token!')
-                except Exception:
-                    raise AuthXException('access_token error!')
-
-                # print(json.dumps(header, indent=4, separators=(',', ': ')))
-                # print(json.dumps(payload, indent=4, separators=(',', ': ')))
-
-                # parse out relevant roles
-                if 'resource_access' in payload.keys() and \
-                        str(self.client_id) in payload["resource_access"].keys() and \
-                        'roles' in payload["resource_access"][self.client_id].keys():
-                    roles = payload["resource_access"][self.client_id]["roles"]
-                    print(roles)
-
-                    # provide access to this token's
-                    # roles via flask 'global'
-                    if 'auth' not in g:
-                        raise AuthXException('Missing roles !')
-
-                    g.authn = {}
-                    g.authn['roles'] = roles
-
-            else:
-                raise AuthXException('Malformed access_token !')
+    def verify_token_required(self):
+        if request.headers.get("Authorization"):
+            self.verify_token()
         else:
             raise AuthXException('Missing access_token !')
+
+    def verify_token(self):
+        # Assume is Bearer token
+        authz_str_split = request.headers.get("Authorization").split(' ')
+        if len(authz_str_split) > 1:
+            token_str = authz_str_split[1]
+            # print(token_str)
+
+            # use idp public_key to validate and parse inbound token
+            try:
+                payload = jwt.decode(token_str, self.public_key, algorithms=[self.oidc_alg], audience="account")
+                # header = jwt.get_unverified_header(token_str)
+            except jwt.exceptions.ExpiredSignatureError:
+                raise AuthXException('Expired access_token!')
+            except Exception:
+                raise AuthXException('access_token error!')
+
+            # print(json.dumps(header, indent=4, separators=(',', ': ')))
+            # print(json.dumps(payload, indent=4, separators=(',', ': ')))
+
+            # parse out relevant roles
+            if 'resource_access' in payload.keys() and \
+                    str(self.client_id) in payload["resource_access"].keys() and \
+                    'roles' in payload["resource_access"][self.client_id].keys():
+                roles = payload["resource_access"][self.client_id]["roles"]
+                print(roles)
+
+                # provide access to this token's
+                # roles via flask 'global'
+                if 'auth' not in g:
+                    raise AuthXException('Missing roles !')
+
+                g.authn = {}
+                g.authn['roles'] = roles
+
+        else:
+            raise AuthXException('Malformed access_token !')
 
 
 class AuthXException(Exception):
