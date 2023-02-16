b66d83dcee6b1f7a46cbf9413d4fa0a53dbffdf7
David Lougheed <david.lougheed@gmail.com>
2023-02-15 19:02:29 -0500

    fix: wording on wildcard fn type error msg

--- bento_lib/search/postgres.py
+++ bento_lib/search/postgres.py
@@ -423,7 +423,7 @@ def _wildcard(args: Tuple[q.AST, q.AST], params: tuple, _schema: JSONSchema, _in
         return sql.Placeholder(), (*params, wcs.format(args[0].value.replace("%", r"\%")))
     except AttributeError:
         # Can happen with non-string argument to #_wc, which will throw on .replace(...)
-        raise TypeError(f"Type-invalid use of binary function {q.FUNCTION_HELPER_WC}")
+        raise TypeError(f"Type-invalid use of function {q.FUNCTION_HELPER_WC}")
 
 
 def get_relation(resolve: Tuple[q.Literal, ...], schema: JSONSchema):
