05adc7f4a56afa3fdbad00241cad54e7f2b535ab
David Lougheed <david.lougheed@gmail.com>
2023-05-09 16:50:46 -0400

    feat: fastapi auth middleware (wip)

---
+++ bento_lib/auth/fastapi_middleware.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request, Response, status
+from fastapi.responses import JSONResponse
+
+from bento_lib.responses.errors import http_error
+from .exceptions import BentoAuthException
+from .middleware.base import BaseAuthMiddleware
+
+
+class FastApiAuthMiddleware(BaseAuthMiddleware):
+    def attach(self, app: FastAPI):
+        """
+        Attach the middleware to an application. Must be called in order for requests to be checked.
+        :param app: A FastAPI application.
+        """
+        app.middleware("http")(self)
+
+    async def __call__(self, request: Request, call_next):
+        # Set flag saying the request hasn't had its permissions determined yet.
+        request.state.bento_determined_authz = False
+
+        try:
+            res: Response = await call_next(request)
+            if not request.state.bento_determined_authz:
+                # Next in response chain didn't properly think about auth; return 403
+                raise BentoAuthException(status_code=status.HTTP_403_FORBIDDEN, message="Forbidden")
+
+        except BentoAuthException as e:
+            request.state.bento_determined_authz = True
+            return JSONResponse(
+                status_code=e.status_code,
+                content=http_error(e.status_code, e.message, drs_compat=self._drs_compat, sr_compat=self._sr_compat))
+
+        # Otherwise, return the response as normal
+        return res
