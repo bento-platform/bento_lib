d9d5fbf018cd9421c5e140583a9050115c644675
David Lougheed <david.lougheed@gmail.com>
2023-06-02 10:43:51 -0400

    chore: add fastapi error response module

---
+++ bento_lib/responses/fastapi_errors.py
@@ -0,0 +1,33 @@
+import logging
+import traceback
+
+from fastapi import status
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
+from .errors import http_error
+
+__all__ = [
+    "http_exception_handler_factory",
+    "validation_exception_handler",
+]
+
+
+def http_exception_handler_factory(logger: logging.Logger):
+    async def http_exception_handler(_request: Request, exc: HTTPException):
+        code = exc.status_code
+
+        if code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+            logger.error(f"Encountered error:\n{traceback.format_exception(type(exc), exc, exc.__traceback__)}")
+
+        return JSONResponse(http_error(code, exc.detail), status_code=code)
+
+    return http_exception_handler
+
+
+async def validation_exception_handler(_request: Request, exc: RequestValidationError):
+    code = status.HTTP_400_BAD_REQUEST
+    return JSONResponse(
+        http_error(code, *((".".join(e["loc"]) + ": " + e["msg"]) if e.get("loc") else e["msg"] for e in exc.errors())),
+        status_code=code)
