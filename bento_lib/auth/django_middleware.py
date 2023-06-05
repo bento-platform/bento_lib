76842548e3248c75e2a36d29ced9004fd258a2c3
David Lougheed <david.lougheed@gmail.com>
2023-06-05 13:17:14 -0400

    chore: impl async-compat django authz middleware

--- bento_lib/auth/django_middleware.py
+++ bento_lib/auth/django_middleware.py
@@ -1,46 +1,67 @@
 from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 from django.conf import settings
-from django.http import HttpRequest
+from django.http import JsonResponse, HttpRequest, HttpResponse
+from typing import Awaitable, Callable
+
+from ..responses.errors import http_error
+from .exceptions import BentoAuthException
 from .middleware.base import BaseAuthMiddleware
 
-
-def extract_middleware_arguments() -> tuple:
-    middleware_settings = getattr(settings, "BENTO_AUTHZ_MIDDLEWARE_CONFIG", {})
-    return (
-        middleware_settings["AUTHZ_SERVICE_URL"],
-        middleware_settings["OPENID_CONFIG_URL"],
-        middleware_settings["OPENID_AUD"],
-        middleware_settings.get("DISALLOWED_ALGORITHMS", frozenset({})),
-        middleware_settings.get("DRS_COMPAT", False),
-        middleware_settings.get("SR_COMPAT", False),
-        settings.DEBUG,
-        middleware_settings.get("ENABLED", True),
-        middleware_settings.get("LOGGER"),
-    )
+__all__ = [
+    "DjangoAuthMiddleware",
+]
 
 
-class AsyncDjangoMiddleware(BaseAuthMiddleware):
+class DjangoAuthMiddleware(BaseAuthMiddleware):
+    async_capable = True
+    sync_capable = True
+
+    @staticmethod
+    def extract_middleware_arguments() -> tuple:
+        middleware_settings = getattr(settings, "BENTO_AUTHZ_MIDDLEWARE_CONFIG", {})
+        return (
+            middleware_settings["AUTHZ_SERVICE_URL"],
+            middleware_settings["OPENID_CONFIG_URL"],
+            middleware_settings["OPENID_AUD"],
+            middleware_settings.get("DISALLOWED_ALGORITHMS", frozenset({})),
+            middleware_settings.get("DRS_COMPAT", False),
+            middleware_settings.get("SR_COMPAT", False),
+            settings.DEBUG,
+            middleware_settings.get("ENABLED", True),
+            middleware_settings.get("LOGGER"),
+        )
+
     def __init__(self, get_response):
-        # TODO: extract settings from Django settings
-
-        self.get_response = get_response
+        self.get_response: Callable[[HttpRequest], Awaitable[HttpResponse] | HttpResponse] = get_response
         if iscoroutinefunction(self.get_response):
             markcoroutinefunction(self)
 
-        super().__init__(*extract_middleware_arguments())
+        super().__init__(*DjangoAuthMiddleware.extract_middleware_arguments())
+
+    def get_authz_header_value(self, request: HttpRequest) -> str | None:
+        return request.headers.get("Authorization")
+
+    @staticmethod
+    def mark_authz_done(request: HttpRequest):
+        request.bento_determined_authz = True
 
     async def __call__(self, request: HttpRequest):
-        request.bento_determined_authz = False  # Just cram a new property in there... no state object
-        # TODO: impl
-        pass
+        if self.enabled:
+            request.bento_determined_authz = False
 
+        try:
+            response = await self.get_response(request)  # We've just crammed a new property in there... no state object
+            if not request.bento_determined_authz:
+                # Next in response chain didn't properly think about auth; return 403
+                return JsonResponse(
+                    http_error(403, "Forbidden", drs_compat=self._drs_compat, sr_compat=self._sr_compat),
+                    status=403)
 
-class SyncDjangoMiddleware(BaseAuthMiddleware):
-    def __init__(self):
-        # TODO: extract settings from Django settings
-        super().__init__(*extract_middleware_arguments())
+        except BentoAuthException as e:
+            self.mark_authz_done(request)
+            return JsonResponse(
+                http_error(e.status_code, e.message, drs_compat=self._drs_compat, sr_compat=self._sr_compat),
+                status_code=e.status_code)
 
-    def __call__(self, request: HttpRequest):
-        request.bento_determined_authz = False  # Just cram a new property in there... no state object
-        # TODO: impl
-        pass
+        # Otherwise, return the response as normal
+        return response
