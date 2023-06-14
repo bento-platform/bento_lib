5c18914f3455410afff375ce0982da7f3557e18d
David Lougheed <david.lougheed@gmail.com>
2023-06-14 11:56:12 -0400

    chore!(auth): mark requests as authorized when raising other errors

--- bento_lib/responses/fastapi_errors.py
+++ bento_lib/responses/fastapi_errors.py
@@ -8,13 +8,14 @@ from fastapi.responses import JSONResponse
 from starlette.responses import Response
 from typing import Callable
 
+from ..auth.middleware.fastapi import FastApiAuthMiddleware
 from ..auth.exceptions import BentoAuthException
 from .errors import http_error
 
 __all__ = [
     "http_exception_handler_factory",
     "bento_auth_exception_handler_factory",
-    "validation_exception_handler",
+    "validation_exception_handler_factory",
 ]
 
 
@@ -23,8 +24,13 @@ def _log_if_500(logger: logging.Logger, code: int, exc: Exception) -> None:
         logger.error(f"Encountered error:\n{traceback.format_exception(type(exc), exc, exc.__traceback__)}")
 
 
-def http_exception_handler_factory(logger: logging.Logger) -> Callable[[Request, HTTPException], Response]:
-    def http_exception_handler(_request: Request, exc: HTTPException) -> JSONResponse:
+def http_exception_handler_factory(
+    logger: logging.Logger,
+    authz: FastApiAuthMiddleware | None = None,
+) -> Callable[[Request, HTTPException], Response]:
+    def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
+        if authz:
+            authz.mark_authz_done(request)
         code = exc.status_code
         _log_if_500(logger, code, exc)
         return JSONResponse(http_error(code, exc.detail), status_code=code)
@@ -32,8 +38,13 @@ def http_exception_handler_factory(logger: logging.Logger) -> Callable[[Request,
     return http_exception_handler
 
 
-def bento_auth_exception_handler_factory(logger: logging.Logger) -> Callable[[Request, BentoAuthException], Response]:
-    def bento_auth_exception_handler(_request: Request, exc: BentoAuthException) -> JSONResponse:
+def bento_auth_exception_handler_factory(
+    logger: logging.Logger,
+    authz: FastApiAuthMiddleware | None = None,
+) -> Callable[[Request, BentoAuthException], Response]:
+    def bento_auth_exception_handler(request: Request, exc: BentoAuthException) -> JSONResponse:
+        if authz:
+            authz.mark_authz_done(request)
         code = exc.status_code
         _log_if_500(logger, code, exc)
         return JSONResponse(http_error(code, exc.message), status_code=code)
@@ -41,10 +52,16 @@ def bento_auth_exception_handler_factory(logger: logging.Logger) -> Callable[[Re
     return bento_auth_exception_handler
 
 
-def validation_exception_handler(_request: Request, exc: RequestValidationError) -> JSONResponse:
-    code = status.HTTP_400_BAD_REQUEST
-    return JSONResponse(
-        http_error(
-            code,
-            *((".".join(map(str, e["loc"])) + ": " + e["msg"]) if e.get("loc") else e["msg"] for e in exc.errors())),
-        status_code=code)
+def validation_exception_handler_factory(
+    authz: FastApiAuthMiddleware | None = None,
+) -> Callable[[Request, RequestValidationError], Response]:
+    def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+        if authz:
+            authz.mark_authz_done(request)
+        code = status.HTTP_400_BAD_REQUEST
+        return JSONResponse(
+            http_error(
+                code,
+                *((".".join(map(str, e["loc"])) + ": " + e["msg"]) if e.get("loc") else e["msg"] for e in exc.errors())),
+            status_code=code)
+    return validation_exception_handler
