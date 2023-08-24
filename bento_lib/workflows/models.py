9ec3e1082630bea9a23bf4bec491d5db471e8642
David Lougheed <david.lougheed@gmail.com>
2023-08-24 16:25:28 -0400

    chore: rewrite WorkflowSet as a manager class

--- bento_lib/workflows/models.py
+++ bento_lib/workflows/models.py
@@ -1,3 +1,6 @@
+import werkzeug.utils
+
+from collections import defaultdict
 from pydantic import BaseModel
 from typing import Literal
 
@@ -106,14 +109,41 @@ WorkflowInput = (
 )
 
 
+WorkflowType = Literal["ingestion", "analysis", "export"]
+
+
 class WorkflowDefinition(BaseModel):
     name: str
+    type: WorkflowType
     description: str
     file: str
     inputs: list[WorkflowInput]
 
 
-class WorkflowSet(BaseModel):
-    ingestion: dict[str, WorkflowDefinition] = []
-    analysis: dict[str, WorkflowDefinition] = []
-    export: dict[str, WorkflowDefinition] = []
+class WorkflowSet:
+    def __init__(self):
+        self._defs_by_id: dict[str, WorkflowDefinition] = {}
+
+    def add_workflow(self, id_: str, definition: WorkflowDefinition):
+        if id_ in self._defs_by_id:
+            raise ValueError(f"Workflow with id {id_} already exists")
+
+        self._defs_by_id[id_] = definition
+
+    def get_workflow(self, id_: str) -> WorkflowDefinition | None:
+        return self._defs_by_id.get(id_)
+
+    def get_workflow_resource(self, id_: str) -> str:
+        return werkzeug.utils.secure_filename(self.get_workflow(id_).file)
+
+    def workflow_exists(self, id_: str) -> bool:
+        return id_ in self._defs_by_id
+
+    def workflow_dicts_by_type_and_id(self) -> dict[str, dict[str, dict]]:
+        by_type = defaultdict(dict)
+        for id_, wf in self._defs_by_id.items():
+            by_type[wf.type][id_] = wf.model_dump(mode="json")
+        return dict(by_type)
+
+    def workflow_dicts_by_id(self) -> dict[str, WorkflowDefinition]:
+        return {k: v.model_dump(mode="json") for k, v in self._defs_by_id.items()}
