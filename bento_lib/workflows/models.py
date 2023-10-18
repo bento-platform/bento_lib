a0b3cc1ceaeadea28fca3ba03a6151a3b6b8ba8f
David Lougheed <david.lougheed@gmail.com>
2023-10-18 14:37:37 -0400

    feat: add workflow tags support

--- bento_lib/workflows/models.py
+++ bento_lib/workflows/models.py
@@ -130,6 +130,7 @@ class WorkflowDefinition(BaseModel):
     type: WorkflowType  # One of a few pre-defined values for categorizing workflow type/purpose
     description: str  # Human-readable workflow description
     file: str  # WDL file name
+    tags: list[str] = []  # Should include data type if relevant
     # Here, inputs defines UI / injected inputs for this workflow. These get transformed into a JSON parameters file
     # which is fed to the WDL workflow description / Cromwell.
     # As such, many of these workflow input types end up mapping to the same WDL type:
