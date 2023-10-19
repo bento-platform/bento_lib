0b1da18a3b5ccba8e2c4a0455ebb8d714f8e0bfc
David Lougheed <david.lougheed@gmail.com>
2023-10-19 15:10:37 -0400

    feat(auth): add some common auth resource helpers

---
+++ bento_lib/auth/resources.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "RESOURCE_EVERYTHING",
+    "build_resource",
+]
+
+# TODO: port Pydantic models from authz instead (when we get pydantic 2)
+
+
+RESOURCE_EVERYTHING = {"everything": True}
+
+
+def build_resource(project: str | None = None, dataset: str | None = None, data_type: str | None = None) -> dict:
+    if project is None:
+        return RESOURCE_EVERYTHING
+
+    res = {"project": project}
+    if dataset:
+        res["dataset"] = dataset
+    if data_type:
+        res["data_type"] = data_type
+
+    return res
