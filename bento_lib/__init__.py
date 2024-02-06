b5b6479a0c0492ec86bb58beef5d5d54d6b05e3e
David Lougheed <david.lougheed@gmail.com>
2024-02-06 10:48:46 -0500

    chore: switch to using Poetry for dependencies

--- bento_lib/__init__.py
+++ bento_lib/__init__.py
@@ -1,5 +1,4 @@
-import configparser
-import os
+from importlib import metadata
 
 from . import auth
 from . import drs
@@ -9,11 +8,5 @@ from . import search
 from . import service_info
 from . import workflows
 
-
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), "package.cfg"))
-
-
-name = config["package"]["name"]
-__version__ = config["package"]["version"]
+__version__ = metadata.version(__name__)
 __all__ = ["__version__", "auth", "drs", "events", "schemas", "search", "service_info", "workflows"]
