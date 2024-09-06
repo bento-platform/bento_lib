05bff693ebdf101f6992c7a41aaebf5f072754f2
David Lougheed <david.lougheed@gmail.com>
2024-09-06 16:03:43 -0400

    feat(ontologies): add ontology terms/resources models/types/instances

--- bento_lib/__init__.py
+++ bento_lib/__init__.py
@@ -4,6 +4,7 @@ from . import apps
 from . import auth
 from . import drs
 from . import events
+from . import ontologies
 from . import schemas
 from . import search
 from . import service_info
@@ -12,5 +13,15 @@ from . import workflows
 
 __version__ = metadata.version(__name__)
 __all__ = [
-    "__version__", "apps", "auth", "drs", "events", "schemas", "search", "service_info", "streaming", "workflows"
+    "__version__",
+    "apps",
+    "auth",
+    "drs",
+    "events",
+    "ontologies",
+    "schemas",
+    "search",
+    "service_info",
+    "streaming",
+    "workflows"
 ]
