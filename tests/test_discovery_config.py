d47b11605f61c5c87a1d17637e103066bc8b1d57
David Lougheed <david.lougheed@gmail.com>
2025-04-07 15:13:08 -0400

    lint

--- tests/test_discovery_config.py
+++ tests/test_discovery_config.py
@@ -3,18 +3,22 @@ from .common import DISCOVERY_CONFIG_PATH
 
 
 def test_overview_charts_def():
-    m1 = overview.OverviewChart.model_validate({
-        "field": "age",
-        "chart_type": "histogram",
-    })
+    m1 = overview.OverviewChart.model_validate(
+        {
+            "field": "age",
+            "chart_type": "histogram",
+        }
+    )
 
     assert m1.field == "age"
     assert m1.chart_type == "histogram"
 
-    m2 = overview.OverviewChart.model_validate({
-        "field": "map",
-        "chart_type": "choropleth",
-    })
+    m2 = overview.OverviewChart.model_validate(
+        {
+            "field": "map",
+            "chart_type": "choropleth",
+        }
+    )
 
     assert m2.field == "map"
     assert m2.chart_type == "choropleth"
