c00f762166acbd7920d71eb57a2e3e1fb1438d03
David Lougheed <david.lougheed@gmail.com>
2025-04-14 10:38:47 -0400

    chore(discovery): re-export DiscoveryConfig model

--- bento_lib/discovery/__init__.py
+++ bento_lib/discovery/__init__.py
@@ -1,4 +1,7 @@
 from . import helpers
 from . import models
+from .models import DiscoveryConfig
 
-__all__ = ["helpers", "models"]
+# Re-export DiscoveryConfig, since it's an important model which will be imported frequently.
+
+__all__ = ["helpers", "models", "DiscoveryConfig"]
