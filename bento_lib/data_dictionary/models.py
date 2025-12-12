92e1e40dc83bd989552f5ddd86d44d96d453b4cf
David Lougheed <david.lougheed@gmail.com>
2025-12-12 11:20:14 -0500

    data dictionary: split into multiple files

--- bento_lib/data_dictionary/models.py
+++ bento_lib/data_dictionary/models.py
@@ -1,27 +1,13 @@
 from pydantic import BaseModel, Field, computed_field
 from typing import Literal
 
+from .types import PossiblyI18nText
+from .utils import i18n_value
+
 __all__ = [
     "DataDictionaryField",
     "DataDictionary",
 ]
-
-
-type PossiblyI18nText = str | dict[str, str]
-
-
-def i18n_value(v: PossiblyI18nText, lang: str, default: str = "") -> str:
-    """
-    TODO
-    :param v: Possibly-internationalized text (either dictionary of ISO 3166-1 alpha-2 --> text, or just plain text.)
-    :param lang: Language (ISO 3166-1 alpha-2)
-    :param default: Default value if a language key is not present in an internationalized text dictionary.
-    :return: The text value, internationalized if possible.
-    """
-    if isinstance(v, str):
-        return v
-    v_vals = tuple(v.values())
-    return v.get(lang, v_vals[0] if v_vals else default)
 
 
 class DataDictionaryField(BaseModel):
