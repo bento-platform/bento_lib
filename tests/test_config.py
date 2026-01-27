15bcf544b123ca8a3144f95f88e8db3d10bee14a
David Lougheed <david.lougheed@gmail.com>
2026-01-27 13:50:43 -0500

    test: add missing tests for new branches

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,13 +3,16 @@ import os
 import pytest
 from bento_lib.config.pydantic import BentoBaseConfig, BentoFastAPIBaseConfig
 from bento_lib.service_info.helpers import build_service_info_from_pydantic_config
-
+from bento_lib.service_info.types import BentoExtraServiceInfo, GA4GHServiceType
 
 TEST_CONFIG_VALUES = dict(
     service_id="test",
     service_name="Test",
     bento_authz_service_url="https://authz.local/",
 )
+
+TEST_BENTO_SERVICE_INFO: BentoExtraServiceInfo = {"serviceKind": "asdf", "dataService": False}
+TEST_SERVICE_TYPE: GA4GHServiceType = {"group": "ca.c3g.bento", "artifact": "beacon", "version": "1.0.0"}
 
 
 def test_base_pydantic_config():
@@ -45,8 +48,22 @@ async def test_build_service_info_for_config():
     res = await build_service_info_from_pydantic_config(
         BentoBaseConfig.model_validate(TEST_CONFIG_VALUES),
         logging.getLogger(__name__),
-        {"serviceKind": "asdf", "dataService": False},
-        {"group": "ca.c3g.bento", "artifact": "beacon", "version": "1.0.0"},
+        TEST_BENTO_SERVICE_INFO,
+        TEST_SERVICE_TYPE,
         "1.2.0",
     )
     assert res
+
+
+@pytest.mark.asyncio
+async def test_build_service_info_for_config_with_description():
+    # Same as above, but with a service description set
+    desc = "This is a pretend service"
+    res = await build_service_info_from_pydantic_config(
+        BentoBaseConfig.model_validate({**TEST_CONFIG_VALUES, "service_description": desc}),
+        logging.getLogger(__name__),
+        TEST_BENTO_SERVICE_INFO,
+        TEST_SERVICE_TYPE,
+        "1.2.0",
+    )
+    assert res["description"] == desc
