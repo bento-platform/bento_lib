cb09661aaed6e8e5743c918413c2166a237be831
David Lougheed <david.lougheed@gmail.com>
2026-08-31 17:10:19 -0400

    add descriptions to disease fields (for now)

--- bento_lib/clinphen/_fields.py
+++ bento_lib/clinphen/_fields.py
@@ -1,4 +1,5 @@
 from operator import not_
+from typing import Any
 
 from pydantic import Field
 
@@ -9,6 +10,8 @@ __all__ = [
     "FIELD_DICT_OR_EMPTY",
     "FIELD_LIST_OR_EMPTY",
     "FIELD_NULLABLE",
+    "field_list_or_empty",
+    "field_nullable",
     "field_name_to_camel",
 ]
 
@@ -17,6 +20,14 @@ FIELD_BLANKABLE = Field(default="", exclude_if=eq_blank)
 FIELD_DICT_OR_EMPTY = Field(default_factory=dict, exclude_if=not_)
 FIELD_LIST_OR_EMPTY = Field(default_factory=list, exclude_if=not_)
 FIELD_NULLABLE = Field(default=None, exclude_if=is_none)
+
+
+def field_list_or_empty(description: str | None = None) -> Any:
+    return Field(default_factory=list, exclude_if=not_, description=description)
+
+
+def field_nullable(description: str | None = None) -> Any:
+    return Field(default=None, exclude_if=is_none, description=description)
 
 
 def field_name_to_camel(value: str) -> str:
--- bento_lib/clinphen/phenopackets/disease.py
+++ bento_lib/clinphen/phenopackets/disease.py
@@ -1,6 +1,8 @@
+from pydantic import Field
+
 from bento_lib.ontologies.models import OntologyClass
 
-from .._fields import FIELD_LIST_OR_EMPTY, FIELD_NULLABLE
+from .._fields import field_list_or_empty, field_nullable
 from .._models import BentoClinPhenModel
 from .time_element import TimeElement
 
@@ -12,11 +14,21 @@ class Disease(BentoClinPhenModel):
     https://phenopacket-schema.readthedocs.io/en/latest/disease.html
     """
 
-    term: OntologyClass
-    excluded: bool | None = FIELD_NULLABLE
-    onset: TimeElement | None = FIELD_NULLABLE
-    resolution: TimeElement | None = FIELD_NULLABLE
-    disease_stage: list[OntologyClass] = FIELD_LIST_OR_EMPTY
-    clinical_tnm_finding: list[OntologyClass] = FIELD_LIST_OR_EMPTY
-    primary_site: OntologyClass | None = FIELD_NULLABLE
-    laterality: OntologyClass | None = FIELD_NULLABLE
+    term: OntologyClass = Field(..., description="Ontology class representing the disease.")
+    excluded: bool | None = field_nullable(
+        description="Whether the disease was observed or not. Negates the disease if true."
+    )
+    onset: TimeElement | None = field_nullable(description="The age or time of onset of the disease.")
+    resolution: TimeElement | None = field_nullable(
+        description="The age or time of resolution of (abatement/recovery from) the disease."
+    )
+    disease_stage: list[OntologyClass] = field_list_or_empty(
+        description="List of ontology classes representing the disease stage."
+    )
+    clinical_tnm_finding: list[OntologyClass] = field_list_or_empty(
+        description="List of ontology classes representing the tumour TNM score, if describing cancer."
+    )
+    primary_site: OntologyClass | None = field_nullable(description="The primary site of the disease.")
+    laterality: OntologyClass | None = field_nullable(
+        description="Laterality (left/right) of the diagnosis, if applicable."
+    )
