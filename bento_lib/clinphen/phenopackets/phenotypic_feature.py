d762797966769e2dd69e7d6cf2a6ba12a474e1a9
David Lougheed <david.lougheed@gmail.com>
2026-08-31 15:20:38 -0400

    complete the phenotypicfeature model

--- bento_lib/clinphen/phenopackets/phenotypic_feature.py
+++ bento_lib/clinphen/phenopackets/phenotypic_feature.py
@@ -1,12 +1,24 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
 
 from bento_lib.ontologies.models import OntologyClass
-from bento_lib.utils.operators import eq_blank
+
+from .._fields import FIELD_BLANKABLE, FIELD_LIST_OR_EMPTY, FIELD_NULLABLE
+from .._models import BentoClinPhenModel
+from .evidence import Evidence
+from .time_element import TimeElement
 
 __all__ = ["PhenotypicFeature"]
 
 
-class PhenotypicFeature(BaseModel):
-    description: str = Field(default="", exclude_if=eq_blank)
+class PhenotypicFeature(BentoClinPhenModel):
+    """
+    https://phenopacket-schema.readthedocs.io/en/latest/phenotype.html
+    """
+    description: str = FIELD_BLANKABLE
     type: OntologyClass
     excluded: bool = Field(default=False)
+    severity: OntologyClass | None = FIELD_NULLABLE
+    modifiers: list[OntologyClass] = FIELD_LIST_OR_EMPTY
+    onset: TimeElement | None = FIELD_NULLABLE
+    resolution: TimeElement | None = FIELD_NULLABLE
+    evidence: list[Evidence] = FIELD_LIST_OR_EMPTY
