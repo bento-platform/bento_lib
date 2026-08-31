7e79503abe81bd6031142483963fe9eca1d8d727
David Lougheed <david.lougheed@gmail.com>
2026-08-31 16:39:51 -0400

    add clinphen family/pedigree models

---
+++ bento_lib/clinphen/phenopackets/pedigree.py
@@ -0,0 +1,30 @@
+from typing import Literal
+
+from pydantic import Field
+
+from .._models import BentoClinPhenModel
+from .individual import Sex
+
+__all__ = ["AffectedStatus", "Person", "Pedigree"]
+
+type AffectedStatus = Literal["MISSING", "UNAFFECTED", "AFFECTED"]
+
+
+class Person(BentoClinPhenModel):
+    """
+    https://phenopacket-schema.readthedocs.io/en/latest/pedigree.html#person
+    """
+
+    family_id: str = Field(..., min_length=1)
+    individual_id: str = Field(..., min_length=1)
+    paternal_id: str = Field(..., min_length=1)
+    maternal_id: str = Field(..., min_length=1)
+    sex: Sex
+    affected_status: AffectedStatus
+
+
+class Pedigree(BentoClinPhenModel):
+    """
+    https://phenopacket-schema.readthedocs.io/en/latest/pedigree.html#pedigree
+    """
+    persons: list[Person] = Field(..., min_length=1)
